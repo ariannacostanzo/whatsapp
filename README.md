10983670c585fe2bbb481bf5af7a963e162f132e
ariannacostanzo <costanzo.arianna@outlook.it>
2024-05-10 11:29:42 +0200

    changed data according to new theme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Quest'app è un'imitazione di Whatsapp gestita su Vue. Da la possibilità di chattare con degli utenti che risponderanno in maniera casuale con emoticon oppure frasi. 
-Ho implementato anche la possibilità di inviare delle foto e cancellare i messaggi. 
+Ho implementato anche la possibilità di inviare delle foto e cancellare i messaggi, nonchè filtrare le chat o i messaggi in chat ed eliminarla completamente
--- js/data.js
+++ js/data.js
@@ -1,192 +1,191 @@
 const data = {
   user: {
-    name: 'Mikasa',
-    avatar: '_io'
+    name: "Mikasa",
+    avatar: "_io",
   },
   contacts: [
     {
       id: 1,
-      name: 'Sasha',
-      avatar: '_1',
+      name: "Sasha",
+      avatar: "_1",
       messages: [
         {
           id: 1,
-          date: '10/01/2020 15:30:55',
-          text: 'Ho una patata qui con me',
-          status: 'sent'
+          date: "10/01/2022 15:30:55",
+          text: "Ho una patata qui con me",
+          status: "sent",
         },
         {
           id: 2,
-          date: '10/01/2020 15:50:00',
-          text: 'La vuoi?',
-          status: 'sent'
+          date: "10/01/2022 15:50:00",
+          text: "La vuoi?",
+          status: "sent",
         },
         {
           id: 3,
-          date: '10/01/2020 16:15:22',
-          text: 'Sto arrivando!!!!!!!',
-          status: 'received'
-        }
+          date: "10/01/2022 16:15:22",
+          text: "Sto arrivando!!!!!!!",
+          status: "received",
+        },
       ],
     },
     {
       id: 2,
-      name: 'Eren',
-      avatar: '_2',
+      name: "Eren",
+      avatar: "_2",
       messages: [
         {
           id: 1,
-          date: '20/03/2020 16:30:00',
-          text: 'Ciao come stai?',
-          status: 'sent'
+          date: "20/03/2020 16:30:00",
+          text: "Eren io cosa sono per te?",
+          status: "sent",
         },
         {
           id: 2,
-          date: '20/03/2020 16:30:55',
-          text: 'Bene grazie! Stasera ci vediamo?',
-          status: 'received'
+          date: "20/03/2020 16:30:55",
+          text: "Tu sei famiglia",
+          status: "received",
         },
         {
           id: 3,
-          date: '20/03/2020 16:35:00',
-          text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-          status: 'sent'
-        }
+          date: "20/03/2020 16:35:00",
+          text: "Seriamente??",
+          status: "sent",
+        },
       ],
     },
     {
       id: 3,
-      name: 'Reiner',
-      avatar: '_3',
+      name: "Reiner",
+      avatar: "_3",
       messages: [
         {
           id: 1,
-          date: '28/03/2020 10:10:40',
-          text: 'La Marianna va in campagna',
-          status: 'received'
+          date: "28/03/2020 10:10:40",
+          text: "Mikasa dì ad Eren che lo sto cercando",
+          status: "received",
         },
         {
           id: 2,
-          date: '28/03/2020 10:20:10',
-          text: 'Sicuro di non aver sbagliato chat?',
-          status: 'sent'
+          date: "28/03/2020 10:20:10",
+          text: "Pensa a nasconderti perchè sto venendo per te",
+          status: "sent",
         },
         {
           id: 3,
-          date: '28/03/2020 16:15:22',
-          text: 'Ah scusa!',
-          status: 'received'
-        }
+          date: "28/03/2020 16:15:22",
+          text: "Oh no 😅",
+          status: "received",
+        },
       ],
     },
     {
       id: 4,
-      name: 'Levi',
-      avatar: '_4',
+      name: "Levi",
+      avatar: "_4",
       messages: [
         {
           id: 1,
-          date: '10/01/2020 15:30:55',
-          text: 'Lo sai che ha aperto una nuova pizzeria?',
-          status: 'sent'
+          date: "29/03/2020 15:30:55",
+          text: "Dov'è quel marmocchio di Eren",
+          status: "received",
         },
         {
           id: 2,
-          date: '10/01/2020 15:50:00',
-          text: 'Si, ma preferirei andare al cinema',
-          status: 'received'
-        }
+          date: "29/03/2020 15:50:00",
+          text: "Ma perchè lo chiedete tutti a me?",
+          status: "sent",
+        },
       ],
     },
     {
       id: 5,
-      name: 'Jean',
-      avatar: '_5',
+      name: "Jean",
+      avatar: "_5",
       messages: [
         {
           id: 1,
-          date: '10/01/2020 15:30:55',
-          text: 'Ricordati di chiamare la nonna',
-          status: 'sent'
+          date: "10/01/2020 15:30:55",
+          text: "Oggi eri bellissima 😍😍😍😍😍",
+          status: "received",
         },
         {
           id: 2,
-          date: '10/01/2020 15:50:00',
-          text: 'Va bene, stasera la sento',
-          status: 'received'
-        }
+          date: "10/01/2020 15:50:00",
+          text: "Ti va di stare con me? 😘😘😘😘😘😘😘😘😘",
+          status: "received",
+        },
       ],
     },
     {
       id: 6,
-      name: 'Yelena',
-      avatar: '_6',
+      name: "Yelena",
+      avatar: "_6",
       messages: [
         {
           id: 1,
-          date: '10/01/2020 15:30:55',
-          text: 'Ciao Claudia, hai novità?',
-          status: 'sent'
+          date: "10/01/2020 15:30:55",
+          text: "Eren è mio, hai capito?",
+          status: "sent",
         },
         {
           id: 2,
-          date: '10/01/2020 15:50:00',
-          text: 'Non ancora',
-          status: 'received'
+          date: "10/01/2020 15:50:00",
+          text: "Ma a me interessa Zeke",
+          status: "received",
         },
         {
           id: 3,
-          date: '10/01/2020 15:51:00',
-          text: 'Nessuna nuova, buona nuova',
-          status: 'sent'
-        }
+          date: "10/01/2020 15:51:00",
+          text: "Allora ok",
+          status: "sent",
+        },
       ],
     },
     {
       id: 7,
-      name: 'Hange',
-      avatar: '_7',
+      name: "Hange",
+      avatar: "_7",
       messages: [
         {
           id: 1,
-          date: '10/01/2020 15:30:55',
-          text: 'Fai gli auguri a Martina che è il suo compleanno!',
-          status: 'sent'
+          date: "10/01/2020 15:30:55",
+          text: "Comandante, che devo fare con Eren? E' sparito 😒",
+          status: "sent",
         },
         {
           id: 2,
-          date: '10/01/2020 15:50:00',
-          text: 'Grazie per avermelo ricordato, le scrivo subito!',
-          status: 'received'
-        }
+          date: "10/01/2020 15:50:00",
+          text: "Non ti preoccupare, ora lo andiamo a cercare",
+          status: "received",
+        },
       ],
     },
     {
       id: 8,
-      name: 'Armin',
-      avatar: '_8',
+      name: "Armin",
+      avatar: "_8",
       messages: [
         {
           id: 1,
-          date: '10/01/2020 15:30:55',
-          text: 'Ciao, andiamo a mangiare la pizza stasera?',
-          status: 'received'
+          date: "10/01/2020 15:30:55",
+          text: "Ci vediamo dopo che finisco quella cosa al porto...",
+          status: "received",
         },
         {
           id: 2,
-          date: '10/01/2020 15:50:00',
-          text: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-          status: 'sent'
+          date: "10/01/2020 15:50:00",
+          text: "Va bene stai attento",
+          status: "sent",
         },
         {
           id: 3,
-          date: '10/01/2020 15:51:00',
-          text: 'OK!!',
-          status: 'received'
-        }
+          date: "10/01/2020 15:51:00",
+          text: "😪😪😪😪😪",
+          status: "received",
+        },
       ],
-    }
-  ]
-
-}
+    },
+  ],
+};
 
